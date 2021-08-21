b18479f7f2d4f1ba86172fe361c23131e38e19f6
Andriy-Lyt <howdy_eh@icloud.com>
2021-08-21 01:30:42 -0400

    handle invalid entries

--- timer1.js
+++ timer1.js
@@ -1,14 +1,28 @@
 const timer = (ar) => {
+  if (ar.length === 0) {
+    return "no timer was set";
+  }
+  for (const elem of ar) {
+    if (elem < 0) {
+      return "can't schedule alarm in the past";
+    }
+    if (typeof elem !== 'number' || elem === " ") {
+      return "invalid time entry";
+    }
+
+  }
+
   for(let delay of ar) {
     setTimeout(() => {
-      console.log("beep after " + delay * 1000 + " sec");
       process.stdout.write('\x07');
+      return ("beep after " + delay * 1000 + " sec");
     }, delay * 1000);
   }
 }
 
 let args = process.argv.slice(2); // console.log("args: ", args);
 
-timer(args);
+console.log(timer(args));
+// console.log(typeof -1);
 
 // process.stdout.write('\x07');
